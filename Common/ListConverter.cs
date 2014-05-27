4726293154fa2d4b8b5fc86bde47432311df8e6b
Nazardo <mauro@nazardo.it>
2014-05-27 16:21:14 +0200

    Lab01

---
+++ Common/ListConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ContosoCookbook.Common
+{
+    class ListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return String.Join("\r\n", (IEnumerable<string>)value);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
